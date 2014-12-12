3e066ab4032ed9084e925aa203ecb8a923d9b1c2
Benoit VALLON <benoitvallon@gmail.com>
2014-12-12 22:09:12 +0100

    Adds variable mappings for amis

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "secret_key" {}
 variable "region" {
     default = "eu-west-1"
 }
+variable "amis" {
+    default = {
+        eu-west-1 = "ami-f0b11187"
+        eu-central-1 = "ami-b43503a9"
+    }
+}
